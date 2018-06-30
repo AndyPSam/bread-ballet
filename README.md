e619e8d0d0c26b953f036bcce24e5393045e69bd
AndyPSam <afb.samuel@gmail.com>
2018-06-30 10:09:10 +0530

    Added pages, styles, js, and image assets

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-<h1>Bread Ballet</h1>
+<a href="https://andypsam.github.io/bread-ballet/"><h1>Bread Ballet</h1></a>
 <p>A sampe website for a hypothetical bread bakery</p>
---
+++ assets/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    if (document.getElementById("menu").style.width > "0rem"){
+        document.getElementById("menu").style.width = "0rem";
+    } else{
+        document.getElementById("menu").style.width = "15.5rem";
+    }
+}
