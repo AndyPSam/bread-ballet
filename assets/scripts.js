1aaf88c5f8c9dfb74185383f193953beceaf4876
AndyPSam <afb.samuel@gmail.com>
2018-07-04 14:00:37 +0530

    Font changes, added content, and some changes in css.

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,3 +5,21 @@ function toggleMenu() {
         document.getElementById("menu").style.width = "15.5rem";
     }
 }
+
+var app = angular.module("ShoppingList", []); 
+app.controller("myCtrl", function($scope) {
+    $scope.products = [];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";    
+        $scope.products.splice(x, 1);
+    }
+});
